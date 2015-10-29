ba299a67e0fc47450a2db31c115ed2f4de24cae4
LeMar Davidson <davidsonl2@email.chop.edu>
2015-10-29 09:34:14 -0400

    Only extract the pcornet schema by default

--- v2 to v3/ETL Scripts/pcornetcdm_to_pcornetcdm_extraction.sh
+++ v2 to v3/ETL Scripts/pcornetcdm_to_pcornetcdm_extraction.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "Beginning Dump of Datbase ${DATABASE}" > log/"$DUMPFILENAME".log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"  pg_dump --host="$EXTRACTDBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" --clean --create  --no-privileges --no-owner --schema="pcornet_cdm"  --schema="pedsnet_cdm" --schema="public" --file=dumps/"${DUMPFILENAME}"
+echo "Beginning Dump of Datbase ${DATABASE}" > log/"$DUMPFILENAME".log && PGPASSWORD="$ETL_DATABASE_USER_PASSWORD"  pg_dump --host="$EXTRACTDBHOSTNAME"   --user="$ETL_DATABASE_USER"  --dbname="$DATABASE" --clean --create  --no-privileges --no-owner --schema="pcornet_cdm"   --file=dumps/"${DUMPFILENAME}"
